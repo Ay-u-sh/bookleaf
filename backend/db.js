08aa245a40b5378d090b37309354c754b430c39c
Ayush Bhande <bhandeayush@gmail.com>
2025-12-06 13:53:00 +0530

    database-config-neon

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,24 @@
 const {Pool} = require('pg');
 
-const pool = new Pool({
-    user:'postgres',
-    password:'123',
-    host:'localhost',
-    port:'5432',
-    database:'appointmentbooking',
-    connectionTimeoutMillis:30000,
-    idleTimeoutMillis:30000,
-    max:5
-})
+// const pool = new Pool({
+//     user:'postgres',
+//     password:'123',
+//     host:'localhost',
+//     port:'5432',
+//     database:'appointmentbooking',
+//     connectionTimeoutMillis:30000,
+//     idleTimeoutMillis:30000,
+//     max:5
+// })
 
 // pool.on('error',)
-
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    },
+    connectionTimeoutMillis: 30000,
+    idleTimeoutMillis: 30000,
+    max: 5
+  });
 module.exports = pool;
